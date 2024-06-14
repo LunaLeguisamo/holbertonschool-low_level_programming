ae3bac11f9903847dfd6c298344feb5ddc967d13
Luna Leguisamo <9356@holbertonstudents.com>
2024-06-14 19:21:23 +0000

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - main
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+        char alphabet;
+
+        for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+        {
+                putchar(alphabet);
+        }
+                putchar('\n');
+
+        return (0);
+}
