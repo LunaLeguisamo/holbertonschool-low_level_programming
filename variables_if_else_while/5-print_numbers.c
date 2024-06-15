9de6882d8fff2f16d71c67fe903ab498e793b530
Luna Leguisamo <9356@holbertonstudents.com>
2024-06-15 18:55:08 +0000

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - main
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int numbers;
+
+	for (numbers < 10; numbers++;)
+	{
+		putchar(numbers);
+	}
+
+	return (0);
+}
