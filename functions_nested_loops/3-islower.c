514a9464f32184be04e2445d800433929eb65b9e
Luna Leguisamo <9354@holbertonstudents.com>
2024-06-17 17:33:17 +0000

    3-islower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - main
+ *
+ * Return: 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+	return (0);
+}
