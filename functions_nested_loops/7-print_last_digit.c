d60d4e3f318a39c0f71f006aadeb44c8f16d5346
Luna Leguisamo <9354@holbertonstudents.com>
2024-06-17 19:07:00 +0000

    7-print_last_digit.c

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+
+#include "main.h"
+
+/**
+ * print_last_digit - main
+ * Return: 0
+ * @a: a number
+ */
+
+int print_last_digit(int a)
+{
+	if (a < 0)
+	{
+		a = -1 * (a % 10);
+		_putchar(a + '0');
+		return (a);
+	}
+	else 
+	{
+		a = a % 10;
+		_putchar(a + '0');
+		return (a);
+	}
+}
