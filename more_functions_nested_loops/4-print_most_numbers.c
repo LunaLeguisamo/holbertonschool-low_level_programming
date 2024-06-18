c8eccd2c3a1f53d7d74a8f4e2cf170324ed38eda
Luna Leguisamo <9356@holbertonstudents.com>
2024-06-18 15:12:49 +0000

    4-print_most_numbers

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function that prints the numbers,
+ * from 0 to 9, except 2 and 4.
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		if (n != 2 && n != 4)
+		_putchar('0' + n);
+	}
+
+		_putchar('\n');
+}
