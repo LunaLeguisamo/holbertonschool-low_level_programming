ade5339eb7bc55df87e91ac4990b2c08435b0e00
Luna Leguisamo <9356@holbertonstudents.com>
2024-06-18 15:04:29 +0000

    3-print_numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints the numbers, from 0 to 9.
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar('0' + n);
+	}
+
+		_putchar('\n');
+}
