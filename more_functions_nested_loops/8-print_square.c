1167e7cab7646c1ccf99c4852183f04c1cdadc1a
Luna Leguisamo <9356@holbertonstudents.com>
2024-06-18 18:56:37 +0000

    8-squares

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -8,14 +8,17 @@
 
 void print_square(int size)
 {
-	int x = 0;
-
+	int x, y = 0;
 
 	if (size > 0)
 	{
 		for (x = 0; x < size ; x++)
 		{
+			for (y = 0; y < size; y++)
+			{
 			_putchar('#');
+			}
+			_putchar('\n');
 		}
 	}
 	else
