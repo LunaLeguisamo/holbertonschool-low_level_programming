f73fa74d29b7e24cd5e8ac2e4a498005c03e201b
Luna Leguisamo <9356@holbertonstudents.com>
2024-06-18 14:46:39 +0000

    1-isdigit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isdigit - main
+ *
+ * Return: 0
+ *
+ * @c: Returns 1 if the input is a digit
+ * @c: Returns 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+	return (0);
+}
