9ccd4b626eed359536e7df749b72e22dde11550c
Luna Leguisamo <9356@holbertonstudents.com>
2024-06-18 19:55:20 +0000

    9-fizz_buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - main
+ * Return: 0
+ * @n: number
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz ");
+			if (n == 100)
+				printf("\n");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+	}
+	return (0);
+}
