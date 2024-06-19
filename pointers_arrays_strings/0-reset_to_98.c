3d116cf251de6ca7df2d3cca41320f6b34f6238b
Luna Leguisamo <9356@holbertonstudents.com>
2024-06-19 17:32:50 +0000

    0-98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that takes a pointer to an int
+ * Return: 0
+ * @a: a integrer
+ * @*n: a pointer
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
