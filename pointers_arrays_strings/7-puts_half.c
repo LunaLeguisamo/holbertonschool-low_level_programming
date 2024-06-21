699792c85ffa37e3d8112a978d736ebb793b153c
Luna Leguisamo <9356@holbertonstudents.com>
2024-06-21 14:55:42 +0000

    7-puts_half

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -38,7 +38,7 @@ void puts_half(char *str)
 	len = _strlen(str);
 	i = len / 2;
 	n = (len - 1) / 2;
-	for (counter = i; counter < len; counter++)
+	for (counter = i - 1; counter < len; counter++)
 	{
 		if (len % 2 != 0)
 		{
