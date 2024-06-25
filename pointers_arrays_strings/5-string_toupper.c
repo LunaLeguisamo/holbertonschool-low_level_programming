605217111a8aa18db877263a853c5c9cb195563f
Luna Leguisamo <9356@holbertonstudent.com>
2024-06-25 01:56:19 +0000

    5-string_toupper

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * _strlen -  function that returns the length of a string
+ * Return: (counter)
+ * @s: a pointer
+ */
+
+int _strlen(char *s)
+{
+	int counter;
+
+	counter = 0;
+	while (s[counter] != '\0')
+	{
+		counter++;
+	}
+	return (counter);
+}
+
+/**
+ * *string_toupper - function that changes all lowercase letters of a string to uppercase
+ *
+ * Return: nothing.
+ */
+
+char *string_toupper(char *p)
+{
+	int counter;
+
+	counter= 0;
+	while (p[counter])
+	{
+		if (p[counter] >= 'a' && p[counter] <= 'z')
+			p[counter] ^= 32;
+
+		counter++;
+	}
+
+	return (p);
+}
