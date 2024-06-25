8db32caa4bc80ff09cfdc77c7c5cbb95d1f4ce6e
Luna Leguisamo <9356@holbertonstudent.com>
2024-06-25 01:57:42 +0000

    5-string_toupper

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -19,7 +19,8 @@ int _strlen(char *s)
 }
 
 /**
- * *string_toupper - function that changes all lowercase letters of a string to uppercase
+ * *string_toupper - function that changes all lowercase
+ * letters of a string to uppercase
  *
  * Return: nothing.
  */
@@ -28,7 +29,7 @@ char *string_toupper(char *p)
 {
 	int counter;
 
-	counter= 0;
+	counter = 0;
 	while (p[counter])
 	{
 		if (p[counter] >= 'a' && p[counter] <= 'z')
