2593bf202089b2208355180bb5ecf93bac93fac0
Luna Leguisamo <5396@holbertonstudents.com>
2024-06-26 01:01:08 +0000

    2-strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * *_strchr - function that locates a character in a string
+ * @s: a string
+ * @c: a letter
+ * Return: a pointer to the memory area s
+ */
+
+char *_strchr(char *s, char c)
+{
+	int counter;
+	int len = _strlen(s);
+	char *p;
+
+	for (counter = 0; counter < len; counter++)
+	{
+		if (s[counter] == c)
+		{
+			p = &s[counter];
+			return (p);
+		}
+	}
+	return (0);
+}
