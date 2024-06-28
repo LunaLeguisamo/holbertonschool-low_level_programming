f47a2cb40bf3d4537034d7900712196c52f4910d
Luna Leguisamo <9356@holbertonstudents.com>
2024-06-28 15:56:14 +0000

    6-prime

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,6 +1,24 @@
 #include "main.h"
 
 /**
+ * mult - function that search if a number is prime
+ * @n: number base
+ * Return: 1 if the input integer is a prime number, otherwise return 0
+ */
+
+int mult(int n)
+{
+	if (n % n == 0 && n % 1 == 0 && n > 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (mult(n + 1));
+	}
+}
+
+/**
  * is_prime_number - function that returns the natural square root of a number
  * @n: number base
  * Return: 1 if the input integer is a prime number, otherwise return 0
@@ -8,10 +26,9 @@
 
 int is_prime_number(int n)
 {
-	if (n % n == 0 && n % 1 == 0 && n > 0)
+	if (n > 1)
 	{
-		return (1);
-		is_prime_number(n);
+		return (mult(n));
 	}
 	else
 	{
