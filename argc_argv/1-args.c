18b675430425478fe01c8b2f4da3a5eac4a66947
Luna Leguisamo <9356@holbertonstudents.com>
2024-06-29 17:07:11 +0000

    1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @argc: count of the arguments supplied to the program
+ * @argv: array of pointers to the strings which are those arguments
+ * Return: Always 0.
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
