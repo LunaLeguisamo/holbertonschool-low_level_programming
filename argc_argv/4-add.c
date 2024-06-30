07780ed1f35f75e3b2efb16326a4b5ac5b8b5030
Luna Leguisamo <9356@holbertonstudents.com>
2024-06-30 00:50:36 +0000

    4-add.c

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -24,7 +24,7 @@ int main(int argc,  char **argv)
 			if (!(isdigit(argv[counter][counter2])))
 			{
 				printf("Error\n");
-				return (0);
+				return (1);
 			}
 		}
 	}
@@ -32,7 +32,6 @@ int main(int argc,  char **argv)
 	if (argc == 1)
 	{
 		printf("0\n");
-		return (1);
 	}
 
 	if (argc > 1)
