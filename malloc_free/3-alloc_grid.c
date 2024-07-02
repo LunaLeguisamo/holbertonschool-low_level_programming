faf2bdd4f1701b99e8bad864e08ca9e96184b5c7
Luna Leguisamo <9356@holbertonstudents.com>
2024-07-02 13:49:05 +0000

    3-alloc_grid

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,46 @@
+i#include "main.h"
+#include <stdlib.h>
+#include "_strlen.c"
+
+/**
+ * alloc_grid - Allocates a 2D array of integers.
+ * @width: The width of the grid.
+ * @height: The height of the grid.
+ *
+ * This function dynamically allocates memory for a 2D array of integers
+ * with the specified width and height. Returns a pointer to the allocated
+ * grid on success, or NULL if memory allocation fails.
+ *
+ * Return: On success, returns a pointer to the allocated grid. If memory
+ * allocation fails or width/height are not positive, returns NULL.
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int counter;
+	int counter1;
+	int **grid;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = malloc(height * sizeof(int *));
+
+	if (grid == NULL)
+		return (NULL);
+
+	for (counter = 0; counter < height; counter++)
+	{
+		grid[counter] = malloc(width * sizeof(int));
+	}
+
+	for (counter = 0; counter < height; counter++)
+	{
+		for (counter1 = 0; counter1 < width; counter1++)
+		{
+			grid[counter][counter1] = 0;
+		}
+	}
+
+	return (grid);
+}
