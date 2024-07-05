208cd5351ab98e5a9dca27d022031a9b5fb5fddc
Luna Leguisamo <9356@holbertonstudents.com>
2024-07-05 23:30:12 +0000

    1-string_nconcat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -14,40 +14,40 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-        unsigned int counter;
-        unsigned int counter1;
-        char *new_str;
-        unsigned int lens1;
-        unsigned int lens2;
+	unsigned int counter;
+	unsigned int counter1;
+	char *new_str;
+	unsigned int lens1;
+	unsigned int lens2;
 
-        if (s1 == NULL)
-                s1 = "";
+	if (s1 == NULL)
+		s1 = "";
 
-        if (s2 == NULL)
-                s2 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-        lens1 = _strlen(s1);
-        lens2 = _strlen(s2);
+	lens1 = _strlen(s1);
+	lens2 = _strlen(s2);
 
-        if (n >= lens2)
-                n = lens2;
+	if (n >= lens2)
+		n = lens2;
 
-        new_str = malloc(lens1 + n + 1);
+	new_str = malloc(lens1 + n + 1);
 
-        if (new_str == NULL)
-                return (NULL);
+	if (new_str == NULL)
+		return (NULL);
 
-        for (counter = 0; counter < lens1; counter++)
-        {
-                new_str[counter] = s1[counter];
-        }
+	for (counter = 0; counter < lens1; counter++)
+	{
+		new_str[counter] = s1[counter];
+	}
 
-        for (counter1 = 0; counter1 < n; counter1++)
-        {
-                new_str[lens1 + counter1] = s2[counter1];
-        }
+	for (counter1 = 0; counter1 < n; counter1++)
+	{
+		new_str[lens1 + counter1] = s2[counter1];
+	}
 
-        new_str[counter + counter1] = '\0';
+	new_str[counter + counter1] = '\0';
 
-        return (new_str);
+	return (new_str);
 }
