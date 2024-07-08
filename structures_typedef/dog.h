e1a5ba9a908766e9222e7ec32d3f59b9816f3ca0
Luna Leguisamo <9356@holbertonstudents.com>
2024-07-08 16:12:22 +0000

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef dogh
+#define dogh
+
+/**
+ * struct dog - struct represents a basic structure
+ * for storing information about a dog
+ * @name: variable with the name of the dog
+ * @age: Variable with the age of the dog
+ * @owner: Variable with the name of the owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
