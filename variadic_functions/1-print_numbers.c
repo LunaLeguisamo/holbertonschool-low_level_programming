fccee630beb58ac6676aa098f88c619fd5c6847f
Luna Leguisamo <9356@holbertonstudents.com>
2024-07-14 18:14:10 +0000

    1-print_numbers.c

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -19,7 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		if (separator == NULL || i == n - 1)
 		{
-			printf("%d",va_arg(print, unsigned int));
+			printf("%d", va_arg(print, unsigned int));
 		}
 		else
 		{
