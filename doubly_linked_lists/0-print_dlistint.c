86644d7ebb73caf1a1f0aa687634eb7253214063
Luna Leguisamo <9356@holbertonstudents.com>
2024-07-30 14:53:25 +0000

    0-print_dlistint.c

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - Prints all the elements of a doubly
+ * linked list of type list_t.
+ * @h: Pointer to the head of the linked list.
+ *
+ * Return: The number of nodes in the doubly linked list.
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *head = h;
+	int counter = 0;
+
+	while (head != NULL)
+	{
+		counter++;
+		printf("%d\n", head->n);
+		head = head->next;
+	}
+	return (counter);
+}
