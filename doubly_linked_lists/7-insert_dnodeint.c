181537928c9bdea67e9cd4974cd641a77c1bf532
Luna Leguisamo <9356@holbertonstudents.com>
2024-08-03 22:30:30 +0000

    7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -48,7 +48,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (nodo->next != NULL)
 	{
-		nodo->prev->next = newNodo;
+		nodo->next->prev = newNodo;
 	}
 
 	nodo->next = newNodo;
