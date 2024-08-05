f1acabd2ec384671edeefbf23ef787aa1c4be2d8
Luna Leguisamo <9356@holbertonstudents.com>
2024-08-05 00:31:07 +0000

    1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,19 +16,18 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content == NULL)
-	{
-
-	}
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
-	writeBytes = write(fd, text_content, sizeof(char));
-	if (writeBytes == -1)
+	if (text_content != NULL)
 	{
-		close(fd);
-		return (-1);
+		writeBytes = write(fd, text_content, strlen(text_content));
+		if (writeBytes == -1)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
 
 	close(fd);
